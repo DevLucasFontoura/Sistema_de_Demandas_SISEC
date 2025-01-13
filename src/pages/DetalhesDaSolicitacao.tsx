2713135511424576822a5e059c01be9eb0a91d44
Lucas Fontoura <lucasfontoura@Lucass-MacBook-Air.local>
2025-01-13 09:59:11 -0300

    Correcao da cor do botao

--- src/pages/DetalhesDaSolicitacao.tsx
+++ src/pages/DetalhesDaSolicitacao.tsx
@@ -447,7 +447,7 @@ function DetalhesDaSolicitacaoPage() {
         </div>
         <button
           onClick={() => navigate('/lista-solicitacoes')}
-          className="flex items-center px-4 py-2 rounded-lg text-gray-300 bg-gradient-to-r from-blue-500/20 to-cyan-500/20 text-white transition-all duration-200 group"
+          className="flex items-center px-4 py-2 rounded-lg text-white bg-gradient-to-r from-blue-500 to-cyan-500 hover:from-blue-600 hover:to-cyan-600 transition-all duration-200"
         >
           <ChevronLeftIcon className="w-5 h-5 mr-2" />
           Voltar
